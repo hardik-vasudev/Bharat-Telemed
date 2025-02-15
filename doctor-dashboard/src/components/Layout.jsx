938a6cb666e6fbd4736d7b93bd59f1f282fbf467
14-Kanika <149942483+14-Kanika@users.noreply.github.com>
2025-02-15 16:39:08 +0530

    Add Layout Component with Sidebar and Topbar

--- doctor-dashboard/src/components/Layout.jsx
+++ doctor-dashboard/src/components/Layout.jsx
@@ -3,17 +3,21 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
+
 const Layout = () => {
   return (
     <div className="flex h-screen w-full">
-      {/* Sidebar */}
+      {/* Sidebar Component */}
       <Sidebar />
 
-      {/* Main Content */}
+      {/* Main Content Area */}
       <div className="flex flex-col w-full">
+        {/* Topbar Component */}
         <Topbar />
+
+        {/* Main Page Content */}
         <div className="flex-grow p-4">
-          <Outlet />
+          <Outlet /> {/* Renders the child components based on the route */}
         </div>
       </div>
     </div>
